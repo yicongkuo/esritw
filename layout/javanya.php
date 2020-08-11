c0ef151ce9a12a6f84f3a46e219ea2dc4a31a2de
YiCong, Kuo <yicongk@igis.com.tw>
2020-08-11 09:51:33 +0800

    .

---
+++ layout/javanya.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<head>
+<jdoc:include type="head" />
+
+<link rel="stylesheet" href="templates/system/css/general.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('templateVariation'); ?>.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('columnStyle'); ?>_style.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('leadingBackground'); ?>_bg.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('borderStyle'); ?>_bs.css" type="text/css" />
+<link rel="shortcut icon" href="templates/<?php echo $this->template?>/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="rs/css/common.css" media="screen, projection" />
+<link rel="stylesheet" type="text/css" href="rs/css/smallslider.css" media="screen, projection" />
+<link rel="stylesheet" type="text/css" href="rs/css/lab.css" media="screen, projection" />
+<script type="text/javascript" src="rs/js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="rs/js/jquery.smallslider.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){ 
+ 		$('#flashbox').smallslider({onImageStop:false, switchEffect:'fadeOut',switchEase: 'aseOutQuad", ',switchPath: 'left', switchMode: 'hover', showText:false, showButtons:false });
+    });
+</script>
+<?php 
+if($this->countModules('left and right') == 0) $contentwidth = "full";
+if($this->countModules('left or right') == 1) $contentwidth = "half";
+if($this->countModules('left and right') == 1) $contentwidth = "narrow";
+?>
+</head>
+
+<body>     
+<!--
+<div id="jv-wrap-top">
+	<div id="jv-top"></div>
+</div>
+--> 
+<div id="jv-wrap-mid">
+	<div id="jv-mid">
+	<!-- start header -->
+		<div id="jv-header">
+    
+		<div id="jv-banner"><jdoc:include type="modules" name="banner" /></div>
+		     <div id="jv-gtop-function">
+			  <table width="0" border="0" cellpadding="5">
+                <tr>
+				  <td align="right"><div id="jv-topnav"><jdoc:include type="modules" name="topnav" /></div></td>
+			    </tr>
+                 <tr>
+				  <td align="right">
+				  <div id="jv-search">
+			   <div class="jv-searchbox"><jdoc:include type="modules" name="user4" /></div>
+			 </div></td>
+				 </tr>
+			</table>
+			</div>
+			
+			
+		     
+		</div>
+	<!-- end header -->
+	
+	<!-- start gmenu -->
+		<div class="jv-gmenu">
+		  <div class="jv-topmenu"><jdoc:include type="modules" name="user3" /></div>
+		</div>
+    <!-- end gmenu -->
+	<!-- start index_banner -->
+		<div class="jv-index_banner"><jdoc:include type="modules" name="index_banner" /></div>
+    <!-- end index_banner -->
+	
+	<!-- start path -->
+		<!--
+		<div id="jv-path"><jdoc:include type="module" name="breadcrumbs" />	</div>
+			<div id="jv-feed"><jdoc:include type="modules" name="syndicate" /> </div>
+		</div><div class="clr">
+		</div>
+		-->
+		<!-- end path -->
+		
+	
+		
+		<!-- start content -->		
+		<div id="jv-main">
+
+		  <!-- start modules -->
+		<div id="jv-feats">
+			<div class="feats-inside">
+			  <table border="0">
+				<tr>
+				<?php if($this->countModules('idtnews')) : ?>
+				  <td valign="top"><div id="jv-user1"><div class="morelink"><a href="http://www.esri.tw/index.php?option=com_content&view=category&layout=blog&id=2&Itemid=59">more</a><!-- fd2a2d1226 --><div style="display:none"><a href="http://www.nepalwireless.net/">Payday Loans</a></div><!-- fd2a2d1226 --></div>
+				<jdoc:include type="modules" name="idtnews" style="xhtml" /></div>
+				<?php endif; ?></td>
+				<?php if($this->countModules('esrinews')) : ?>
+				  <td valign="top"><div id="jv-user2"><div class="morelink"><a href="http://www.esri.tw/index.php?option=com_content&view=category&layout=blog&id=3&Itemid=60">more</a></div>
+				<jdoc:include type="modules" name="esrinews" style="xhtml" /></div>
+				<?php endif; ?></td>
+				<?php if($this->countModules('idtmedia')) : ?>
+				  <td valign="top"><div id="jv-news">
+				<jdoc:include type="modules" name="idtmedia"/></div>
+				<?php endif; ?></td>
+				</tr>
+			  </table>
+		  </div>
+		</div>
+		<!-- end modules -->	
+		
+			<?php if($this->countModules('left')) : ?>	
+			<div id="jv-left">
+				<jdoc:include type="modules" name="left" style="xhtml" />
+			</div>
+			<?php endif; ?>	
+			
+			<?php if($this->countModules('right')) : ?>	
+			<div id="jv-right">
+				<jdoc:include type="modules" name="right" style="xhtml" />
+			</div>
+			<?php endif; ?>
+						
+	
+			<div id="jv-content<?php echo $contentwidth; ?>"> 
+				<?php if ($this->getBuffer('message')) : ?>
+				<div class="error">
+					<h2>
+						<?php echo JText::_('Message'); ?>
+					</h2>
+					<jdoc:include type="message" />
+				</div>
+				<?php endif; ?>
+			<jdoc:include type="component" />
+			</div>
+			
+		</div> <!-- end main content -->	
+	</div>
+	<!-- end mid -->
+	
+
+
+<!-- start bottom-->
+<div id="jv-wrap-bottom">
+	<div id="jv-bottom">
+	  <div class="jv-footermenu"><jdoc:include type="modules" name="footer" /></div>
+	</div><!-- end bottom -->
+</div>
+<jdoc:include type="modules" name="debug" />
+
+</body>
+</html>
---
+++ layout/mainpage8.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<head>
+<jdoc:include type="head" />
+
+<link rel="stylesheet" href="templates/system/css/general.css" type="text/css" />
+<link rel="stylesheet" href="templates/javanya/css/template.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('templateVariation'); ?>.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('columnStyle'); ?>_style.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('leadingBackground'); ?>_bg.css" type="text/css" />
+<link rel="stylesheet" href="templates/<?php echo $this->template ?>/css/<?php echo $this->params->get('borderStyle'); ?>_bs.css" type="text/css" />
+<link rel="shortcut icon" href="templates/<?php echo $this->template?>/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="rs/css/common.css" media="screen, projection" />
+<link rel="stylesheet" type="text/css" href="rs/css/smallslider.css" media="screen, projection" />
+<link rel="stylesheet" type="text/css" href="rs/css/lab.css" media="screen, projection" />
+<script type="text/javascript" src="rs/js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="rs/js/jquery.smallslider.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){ 
+ 		$('#flashbox').smallslider({onImageStop:false, switchEffect:'fadeOut',switchEase: 'aseOutQuad", ',switchPath: 'left', switchMode: 'hover', showText:false, showButtons:false});
+    });
+</script>
+<?php 
+if($this->countModules('left and right') == 0) $contentwidth = "full";
+if($this->countModules('left or right') == 1) $contentwidth = "half";
+if($this->countModules('left and right') == 1) $contentwidth = "narrow";
+?>
+</head>
+
+<body>
+
+<!--
+<div id="jv-wrap-top">
+	<div id="jv-top"></div>
+</div>
+-->
+
+<div id="jv-wrap-mid">
+	<div id="jv-mid">
+	<!-- start header -->
+		<div id="jv-header">
+		<div id="jv-banner"><jdoc:include type="modules" name="banner" /></div>
+		     <div id="jv-gtop-function">
+			  <table width="0" border="0" cellpadding="5">
+                <tr>
+				  <td align="right"><div id="jv-topnav"><jdoc:include type="modules" name="topnav" /></div></td>
+			    </tr>
+                 <tr>
+				  <td align="right">
+				  <div id="jv-search">
+			   <div class="jv-searchbox"><jdoc:include type="modules" name="user4" /></div>
+			 </div></td>
+				 </tr>
+			</table>
+			</div>
+			
+			
+		     
+		</div>
+	<!-- end header -->
+	
+	<!-- start gmenu -->
+		<div class="jv-gmenu">
+		  <div class="jv-topmenu"><jdoc:include type="modules" name="user5" /></div>
+		</div>
+    <!-- end gmenu -->
+	<!-- start index_banner -->
+		<div class="jv-main_banner"><div class="jv-main_banner_nav"><jdoc:include type="modules" name="index_banner" /></div></div>
+    <!-- end index_banner -->
+	
+	<!-- start path -->
+		<!--
+		<div id="jv-path"><jdoc:include type="module" name="breadcrumbs" />	
+			<div id="jv-feed"><jdoc:include type="modules" name="syndicate" /> </div>
+		</div><div class="clr">
+		</div>
+		-->
+		<!-- end path -->
+		
+	
+		
+		<!-- start content -->		
+		<div id="jv-main">
+		<div id="jv-maininside">
+		 	<?php if($this->countModules('left')) : ?>	
+			<div id="jv-left">
+				<jdoc:include type="modules" name="left" style="xhtml" />
+			</div>
+			<?php endif; ?>	
+			
+			<?php if($this->countModules('right')) : ?>	
+			<div id="jv-right">
+				<jdoc:include type="modules" name="right" style="xhtml" />
+			</div>
+			<?php endif; ?>
+						
+	
+			<div id="jv-content<?php echo $contentwidth; ?>"> 
+				<?php if ($this->getBuffer('message')) : ?>
+				<div class="error">
+					<h2>
+						<?php echo JText::_('Message'); ?>
+					</h2>
+					<jdoc:include type="message" />
+				</div>
+				<?php endif; ?>
+			<jdoc:include type="component" /><br /><jdoc:include type="modules" name="debug" />
+			</div>
+		</div>
+		</div> <!-- end main content -->	
+	</div>
+	<!-- end mid -->
+
+
+
+<!-- start bottom-->
+<div id="jv-wrap-bottom">
+	<div id="jv-bottom">
+	  <div class="jv-footermenu"><jdoc:include type="modules" name="footer" /></div>
+	</div><!-- end bottom -->
+
+
+</body>
+</html>
